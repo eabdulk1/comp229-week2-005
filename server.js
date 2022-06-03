fd08acff3a8cc7704c8a9a45cc493243352b3ca8
Eabdulkaliq <sam.abdulkaliq@gmail.com>
2022-06-03 17:12:34 -0400

    week2 complete code

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 const connect = require('connect');
 const app =connect();
+
+function logger (req,res,next){
+console.log(req,res,next);
+
+};
+
+
+
 function helloworld(req,res,next){
 
     res.setHeader('Content-Type','text/plain');
-    res.end('hello world');
+    res.end('Hello World');
 
 }
+
+
+function goodbyeWorld(req,res,next){
+
+    res.setHeader('Content-Type','text/plain');
+    res.end('goodbye World');
+
+}
+app.use(logger);
+app.use('/hello', helloworld);
+app.use('/goodbye', goodbyeWorld);
+
 //first custom middleware
-app.use(helloworld);
-app.listen(300);
 
-console.log('Server is running at http://localhost:300/');
-
+app.listen(3000);
+console.log('Server is running at http://localhost:3000/');
